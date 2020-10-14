41e39dd1d3a8c1bc231d347af9ccb666cc65c2b8
danil0110 <danilmail0110@gmail.com>
2020-10-14 12:15:28 +0300

    "Scroll to top" button implemented

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ document.querySelector('.burger-menu').onclick = () => {
     let body = document.getElementsByTagName('body');
     body[0].classList.toggle('lock');
 }
+
+document.querySelector('.scroll-top').onclick = () => {
+    window.scrollTo(pageYOffset, 0);
+}
+
+window.addEventListener('scroll', function() {
+    if (pageYOffset < 200) {
+        document.querySelector('.scroll-top').classList.remove('visible');
+    }
+    else {
+        if (!document.querySelector('.scroll-top').classList.contains('visible')) {
+            document.querySelector('.scroll-top').classList.add('visible');
+        }
+    }
+})
